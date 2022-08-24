12566a8852e6bc5452f2ab73835193fbb087957f
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-08-23 23:29:15 -0300

    Add index file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Api php cep</title>
+	<script src="main.js"></script>	
+</head>
+<body>
+	<h1>Informe o cep para consulta:</h1>
+	<form>
+		<p>CEP: <input type="text" id="cep"></p>
+		<button id="btn" onclick="callApi()">Consultar</button>
+	</form>
+</body>
+</html>
